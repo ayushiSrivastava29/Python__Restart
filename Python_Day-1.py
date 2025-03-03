9408cd01dd918b11f81dc49b2d4731baa65b81ac
ayushiSrivastava29 <ayushikashyap2001@gmail.com>
2025-03-03 17:24:18 +0530

    first commit

---
+++ Python_Day-1.py
@@ -0,0 +1,44 @@
+# Day -1 Topic--- Variables and Data Types 
+
+print("Hello Everyone", ", Welcome to the Python world.")
+name = "Ayushi" #string 
+rollno  = 21
+Percentage = 98.34
+
+print("My Name is: ",name)
+print("My Percentage is: ",Percentage)
+
+# Data Types
+Name = 'ayushi'
+Name2 = "you"
+Name3 = '''Lovely'''
+
+old= True
+q= None
+
+print(type(old))
+print(type(q))
+
+print(Name)
+print(Name2)
+print(Name3)
+
+#Print sum -- quick program
+# a= 2
+# b=10
+# sum = a+b
+# print(sum)
+
+# Operators in Python 
+
+# Arithmetic Operators - [+,-,*,/,//,%,**]
+
+# Logical Operators - [and , or , not]
+
+# Assignment Operators - [=,+=,-=,*=,/=,//=,%=,**=]
+
+# Relational Operators - [==,!=,>,<,>=,<=]
+
+num = int(input("Enter Some value: "))
+print(type(num),num)
+
